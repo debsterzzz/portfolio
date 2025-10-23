aa6780217ed14b8995149dcb705edd6d7bdaa900
debsterzzz <146525864+debsterzzz@users.noreply.github.com>
2025-10-22 23:44:16 -0700

    Update global.js

--- global.js
+++ global.js
@@ -16,7 +16,8 @@ let pages = [
   { url: '', title: 'Home' },
   { url: 'contact/', title:'Contact'},
   { url: 'projects/', title: 'Projects' },
-  { url: 'resume/', title:'Resume'}
+  { url: 'resume/', title:'Resume'},
+  {url: 'https://github.com/debsterzzz/portfolio', title:'GitHub'}
   // add the rest of your pages here
 ];
 let nav = document.createElement('nav');
