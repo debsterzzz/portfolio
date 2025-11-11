9cb29d4b3b7d14955d93c8a2d1c81cac0085b409
debsterzzz <146525864+debsterzzz@users.noreply.github.com>
2025-11-10 22:40:58 -0800

    meta page active

--- global.js
+++ global.js
@@ -17,7 +17,7 @@ let pages = [
   { url: 'contact/', title:'Contact'},
   { url: 'projects/', title: 'Projects'},
   { url: 'resume/', title:'Resume'},
-  { url: 'Meta/', title:'Meta'},
+  { url: 'meta/', title:'Meta'},
   { url: 'https://github.com/debsterzzz/portfolio', title:'GitHub'}
   // add the rest of your pages here
 ];
